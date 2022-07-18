d60faff3a8bac094d0f0cf38620d5c57fb7a02f7
npddoanh@gmail.com <npddoanh@gmail.com>
2022-07-18 18:33:48 +0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-
 import App from './App';
-
-import {store} from './redux/configStore';
-import './assets/scss/index.scss';
 import './index.css';
 import './scss/index.scss';
-
-
+import './assets/scss/index.scss';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import {store} from './redux/configStore';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
